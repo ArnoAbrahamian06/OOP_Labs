cf21d0f778e1fe4a7be70c3589accf030305db9f
IgorFursov <fursovigor0607@gmail.com>
2025-09-23 02:20:22 +0400

    Тесты для сложных функций

--- src/main/java/org/example/functions/ZeroFunction.java
+++ src/main/java/org/example/functions/ZeroFunction.java
@@ -5,8 +5,4 @@ public class ZeroFunction extends ConstantFunction{
     public ZeroFunction (){
         super(0);
     }
-    @Override
-    public double apply(double x) {
-        return 0;
-    }
 }
