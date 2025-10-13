48909c4370cf468b633c3b8c99ef2915238a77a6
IgorFursov <fursovigor0607@gmail.com>
2025-10-13 23:00:09 +0400

    реализация метода createUnmodifiable в абстрактной фабрике TabulatedFunctionFactory

--- src/main/java/org/example/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/org/example/functions/factory/TabulatedFunctionFactory.java
@@ -1,9 +1,15 @@
 package org.example.functions.factory;
 
 import org.example.functions.TabulatedFunction;
+import org.example.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createUnmodifiable (double[] xValues, double[] yValues){
+        TabulatedFunction function = create(xValues, yValues);
+        return new UnmodifiableTabulatedFunction(function);
+    }
 }
 
 
--- src/test/java/org/example/functions/factory/TabulatedFunctionFactoryTest.java
+++ src/test/java/org/example/functions/factory/TabulatedFunctionFactoryTest.java
@@ -3,11 +3,36 @@ package org.example.functions.factory;
 import org.example.functions.ArrayTabulatedFunction;
 import org.example.functions.LinkedListTabulatedFunction;
 import org.example.functions.TabulatedFunction;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TabulatedFunctionFactoryTest {
+
+    @Test
+    public void testArrayFactoryCreateUnmodifiable() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0, 4.0};
+        double[] yValues = {10.0, 20.0, 30.0, 40.0};
+
+        TabulatedFunction unmodifiableFunction = factory.createUnmodifiable(xValues, yValues);
+
+        // Проверяем, что данные корректны
+        assertEquals(4, unmodifiableFunction.getCount());
+        assertEquals(1.0, unmodifiableFunction.getX(0), 1e-10);
+        assertEquals(10.0, unmodifiableFunction.getY(0), 1e-10);
+        assertEquals(4.0, unmodifiableFunction.getX(3), 1e-10);
+        assertEquals(40.0, unmodifiableFunction.getY(3), 1e-10);
+
+        // Проверяем, что попытка изменить функцию бросает исключение
+        try {
+            unmodifiableFunction.setY(0, 100.0);
+            fail("Expected UnsupportedOperationException");
+        } catch (UnsupportedOperationException e) {
+            // Ожидаемое поведение
+        }
+    }
 
     @Test
     public void testArrayFactoryCreatesArrayTabulatedFunction() {
