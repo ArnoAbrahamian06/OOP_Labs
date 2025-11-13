659400f8fdd1fb913713113799d5d4c2c213e343
abramyanarno2006 <abramyanarno2006@gmail.com>
2025-11-14 00:51:56 +0400

    коммит перед слиянием

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,11 @@ package org.example;
 
 import org.example.functions.ArrayTabulatedFunction;
 import org.example.functions.MathFunction;
+import org.example.functions.SqrFunction;
 
 public class Main {
     public static void main(String[] args) {
-        double[] x1 = {0, 1, 2, 3};
-        double[] y1 = {0, 2, 4, 6};
-        ArrayTabulatedFunction inner = new ArrayTabulatedFunction(x1, y1);
-
-        double[] x2 = {0, 2, 4, 6};
-        double[] y2 = {0, 1, 4, 9};
-        ArrayTabulatedFunction outer = new ArrayTabulatedFunction(x2, y2);
-
-        MathFunction composition = inner.andThen(outer);
-
-        // Проверка точек из исходной области определения
-        composition.apply(0.0);
+        MathFunction function = new SqrFunction();
+        System.out.println(function.apply(7));
     }
 }
